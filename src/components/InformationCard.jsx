0f549d599ff98990347569fe5fd342162ba5072b
Jonas Lindevall <jonas.h.lindevall@gmail.com>
2023-05-08 10:33:14 +0200

    feat: content to explain application

---
+++ src/components/InformationCard.jsx
@@ -0,0 +1,11 @@
+const InformationCard = () => {
+    return (
+        <div>
+            <h1>Velkommen!</h1>
+            <h2>Dette er et lite program som konverterer romertall til vanlige tall!</h2>
+            <h3>I = 1 <br />V = 5 <br />X = 10 <br />L = 50 <br />C = 100 <br />D = 500 <br />M = 1000 <br /> </h3>
+        </div>
+    )
+}
+
+export default InformationCard
